8a7c62dbff8f1afa17de7b569f9b3d5d5c375d54
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-06-11 18:25:47 +0200

    chore: exclude tests directory from git monitoring

--- tests/capitalize.test.js
+++
@@ -1,9 +0,0 @@
-import capitalize from '../src/js/capitalize.js';
-
-test('Capitalize first letter of a string', () => {
-  expect(capitalize('dejan')).toMatch('Dejan');
-});
-
-test('Only first letter should be capitalized', () => {
-  expect(capitalize('deJaN')).toMatch('Dejan');
-});
--- tests/reverseString.test.js
+++
@@ -1,5 +0,0 @@
-import reverseString from '../src/js/reverseString';
-
-test('Return a string reversed', () => {
-  expect(reverseString('dejan')).toMatch('najed');
-});
